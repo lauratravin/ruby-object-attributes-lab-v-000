d4ee025738b313bc253972569f693637575f6bbc
Laura Messa Travin <laura.travin@gmail.com>
2018-02-05 19:56:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
    def initialize(name)
      @name = name
    end
-   def name=(newdogname) #setter
-     @dogname = newdogname
+   def name=(newname) #setter
+     @name = newname
    end
    def name #getter
      @dogname
